ed8f23d96b13731f565e7b24a622f76a677fe01e
chahyunsoo <chahs1012@naver.com>
2025-01-20 18:50:05 +0900

    first commit

---
+++ bootstrap-module/src/main/java/com/bondary/BondaryBackendApplication.java
@@ -0,0 +1,13 @@
+package com.bondary;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BondaryBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BondaryBackendApplication.class, args);
+	}
+
+}
---
+++ bootstrap-module/src/test/java/com/bondary/PeocleBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bondary;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PeocleBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
