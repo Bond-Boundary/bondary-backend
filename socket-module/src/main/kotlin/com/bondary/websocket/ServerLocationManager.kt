8cb73d2c62a7dd9345b88c367d26ed7ff62a75fd
chahyunsoo <chahs1012@naver.com>
2025-02-17 02:18:45 +0900

    feat: pont time update 및 get manager 추가 및 serverIp 매핑 key name 변경

--- socket-module/src/main/kotlin/com/bondary/websocket/ServerLocationManager.kt
+++ socket-module/src/main/kotlin/com/bondary/websocket/ServerLocationManager.kt
@@ -5,27 +5,41 @@ import kotlinx.coroutines.reactor.awaitSingleOrNull
 import org.springframework.beans.factory.annotation.Value
 import org.springframework.data.redis.core.ReactiveRedisTemplate
 import org.springframework.stereotype.Component
+import java.time.LocalDateTime
 
 @Component
 class ServerLocationManager(
-    private val reactiveStringRedisTemplate: ReactiveRedisTemplate<Long, String>,
+    private val reactiveStringRedisTemplate: ReactiveRedisTemplate<String, String>,
     @Value("\${server.host}") private val serverIP: String
 ) {
     suspend fun saveUserServerLocation(userId: Long) {
         reactiveStringRedisTemplate.opsForValue()
-            .set(userId, serverIP)
+            .set("user:server:$userId", serverIP)
             .awaitSingle()
     }
 
     suspend fun getUserServerLocation(userId: Long): String? {
         return reactiveStringRedisTemplate.opsForValue()
-            .get(userId)
+            .get("user:server:$userId")
             .awaitSingleOrNull()
     }
 
     suspend fun removeUserServerLocation(userId: Long) {
         reactiveStringRedisTemplate.opsForValue()
-            .delete(userId)
+            .delete("user:server:$userId")
             .awaitSingle()
+    }
+
+    suspend fun updateLastPongTime(userId: Long) {
+        reactiveStringRedisTemplate.opsForValue()
+            .set("user:pong:$userId", LocalDateTime.now().toString())
+            .awaitSingle()
+    }
+
+    suspend fun getLastPongTime(userId: Long): LocalDateTime? {
+        return reactiveStringRedisTemplate.opsForValue()
+            .get("user:pong:$userId")
+            .awaitSingleOrNull()
+            ?.let { LocalDateTime.now() }
     }
 }
