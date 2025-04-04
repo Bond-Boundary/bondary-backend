600024ac483d243c653497500c47431bfeb6a636
chahyunsoo <chahs1012@naver.com>
2025-04-04 16:18:29 +0900

    feat: Career delete usecase, port 정의 및 service 구현

--- bondary-application/src/main/kotlin/com/bondary/application/career/CareerCommandService.kt
+++ bondary-application/src/main/kotlin/com/bondary/application/career/CareerCommandService.kt
@@ -1,17 +1,21 @@
 package com.bondary.application.career
 
 import com.bondary.application.career.`in`.CreateCareerUseCase
+import com.bondary.application.career.`in`.DeleteCareerUseCase
 import com.bondary.application.career.`in`.ModifyCareerUseCase
 import com.bondary.application.career.out.CareerFunctionPort
 import com.bondary.career.Career
 import com.bondary.support.DomainId
 import org.springframework.stereotype.Service
+import org.springframework.transaction.annotation.Transactional
 
 @Service
 class CareerCommandService(
     private val careerFunctionPort: CareerFunctionPort
 ) : CreateCareerUseCase,
-    ModifyCareerUseCase {
+    ModifyCareerUseCase,
+    DeleteCareerUseCase {
+    @Transactional
     override fun createCareer(command: CreateCareerUseCase.Command): CreateCareerUseCase.Response.Success {
         val create = Career.createCareer(
             memberId = DomainId(command.memberId),
@@ -28,6 +32,7 @@ class CareerCommandService(
         return CreateCareerUseCase.Response.Success(successId)
     }
 
+    @Transactional
     override fun modifyCareer(command: ModifyCareerUseCase.Command): ModifyCareerUseCase.Response.Success {
         val career = careerFunctionPort.getCareer(command.careerId, command.memberId)
         career.modifyCareer(
@@ -42,5 +47,14 @@ class CareerCommandService(
 
         val successId = careerFunctionPort.modify(career)
         return ModifyCareerUseCase.Response.Success(successId)
+    }
+
+    @Transactional
+    override fun deleteCareer(command: DeleteCareerUseCase.Command): DeleteCareerUseCase.Response.Success {
+        val career = careerFunctionPort.getCareer(command.careerId, command.memberId)
+            .also { it.deleteCareer() }
+
+        val successId = careerFunctionPort.delete(career)
+        return DeleteCareerUseCase.Response.Success(successId)
     }
 }
